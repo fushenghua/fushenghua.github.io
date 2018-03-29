5e70405f84d41adebca68220a2a57bc762e931b9
fushenghua <fushenghua2012@126.com>
2018-03-29 15:01:53 +0800

    Site updated: 2018-03-29 15:01:52

--- js/weixin.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-  document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-      // 通过下面这个API隐藏右上角按钮
-      WeixinJSBridge.call('hideOptionMenu');
-      WeixinJsBridge.invoke('disabledShare',{},function(e){
-      　　});
-  });
-});
